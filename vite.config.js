cbba37fa8fdb0858625d782d1678a39d9fdb3d24
Desmond Lyu <desmondlyu@users.noreply.github.com>
2026-06-24 01:06:42 +0800

    fix: add envDir to vite config so .env.local is correctly baked into build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
+  envDir: '../',
   plugins: [react(), viteSingleFile()],
   build: {
     outDir: '../',
